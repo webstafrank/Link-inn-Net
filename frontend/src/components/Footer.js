f532dadc9c02ad93366b2a1756407879dd0c7fea
root <franklyndenish1@gmail.com>
2025-01-24 04:18:22 -0800

    Implementing footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="px-8 py-6 bg-black text-gray-500 text-center">
+      © 2025 Link inn Net. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
+
