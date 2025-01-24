3881918376204d696860bf6752fa926e37430065
root <franklyndenish1@gmail.com>
2025-01-24 04:16:32 -0800

    Adding Hero section

---
+++ frontend/src/components/Herosection.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div className="px-8 py-20 bg-black text-white">
+      <h2 className="text-4xl font-bold text-yellow-500 mb-4">
+        Welcome to Link inn Net
+      </h2>
+      <p className="text-xl text-gray-300 mb-6">
+        Connecting Homes, Empowering Lives.
+      </p>
+      <button className="bg-yellow-500 text-black font-bold px-6 py-3 rounded-lg">
+        Buy Internet
+      </button>
+    </div>
+  );
+};
+
+export default HeroSection;
+
