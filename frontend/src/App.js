277ef145b51a99b23556c32f040e96b53b8e5b1e
root <franklyndenish1@gmail.com>
2025-01-24 04:07:32 -0800

    App components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,20 @@
-// frontend/src/App.js
-import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
 
-const LandingPage = lazy(() => import('./components/LandingPage'));
-const PackagePage = lazy(() => import('./pages/PackagePage'));
-const CommunityPage = lazy(() => import('./pages/CommunityPage'));
-const NotFoundPage = () => <div className="p-6 text-center text-red-500">404 - Page Not Found</div>;
+import React from "react";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import PackagesSection from "./components/PackagesSection";
+import Footer from "./components/Footer";
 
-const App = () => {
-    return (
-        <Router>
-            <Navbar />
-            <main className="min-h-screen">
-                <Suspense fallback={<div className="p-6 text-center">Loading...</div>}>
-                    <Routes>
-                        <Route path="/" element={<LandingPage />} />
-                        <Route path="/packages" element={<PackagePage />} />
-                        <Route path="/community" element={<CommunityPage />} />
-                        <Route path="*" element={<NotFoundPage />} />
-                    </Routes>
-                </Suspense>
-            </main>
-        </Router>
-    );
-};
+function App() {
+  return (
+    <div className="bg-black text-white min-h-screen">
+      <Navbar />
+      <HeroSection />
+      <PackagesSection />
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
 
